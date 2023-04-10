0d6ac8c51603508561ac3b8f789bd6b842c0d097
Jianzhang Peng <pengjianzhang@gmail.com>
2023-04-10 02:09:00 +0000

    multicast config

---
+++ multicast/config.sh
@@ -0,0 +1,5 @@
+echo 0 > /proc/sys/net/ipv4/conf/default/rp_filter
+echo 0 > /proc/sys/net/ipv4/conf/all/rp_filter
+echo 0 > /proc/sys/net/ipv4/conf/eth1/rp_filter
+echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+
